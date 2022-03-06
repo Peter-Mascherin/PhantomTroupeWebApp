1c369217bcaf4f5703960b116e4d8e990dc22381
abdulnmo <abdulnmo@sheridancollege.ca>
2022-03-06 01:00:56 -0500

    layout fix

--- src/app/component/review/review.component.ts
+++ src/app/component/review/review.component.ts
@@ -23,11 +23,11 @@ export class ReviewComponent implements OnInit {
 
   ngOnInit(): void {
     this.reviews = [
-      {name: 'Alex', review: "Exception quality, recommend this company"},
-      {name: 'Moe', review: "Stunned by the quality, love it !!!!"},
-      {name: 'Joe', review: "Material is good and love it"},
-      {name: 'Andy', review: "Best business I've done"},
-      {name: 'Rob', review: "Received it quickly and was not disappointed 5/5"},
+      {name: 'Alex', review: "\"Exception quality, recommend this company\""},
+      {name: 'Moe', review: " \"Stunned by the quality, love it !!!!\" "},
+      {name: 'Joe', review: " \"Material is good and love it\" "},
+      {name: 'Andy', review: " \"Best business I've done\" "},
+      {name: 'Rob', review: " \"Received it quickly, not disappointed 5/5\" "},
      
   ];
 
