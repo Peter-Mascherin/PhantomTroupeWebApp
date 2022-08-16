774221ba55f48125543ec7e7934e01ff8d597c8b
GursikhHajra <hajra@sheridancollege.ca>
2022-08-15 20:00:57 -0400

    adminPage

---
+++ src/app/Admin/admin-page/admin-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-admin-page',
+  templateUrl: './admin-page.component.html',
+  styleUrls: ['./admin-page.component.css']
+})
+export class AdminPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  btnLogin(){
+    this.worng();
+  }
+
+  btnCancel(){
+
+  }
+
+  worng(){ 
+    Swal.fire({
+      icon: 'error',
+      title: 'Input error',
+      text: 'Worng Password or Username',
+    })
+  }
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,17 @@ export class AppComponent {
   title = 'woodstove'; 
 
 
+  constructor(private router: Router) {}
   testfunc()
   {
     console.log("Yea the gallery button works");
   }
+
+  goToAdmin(){
+    console.log('Admin was clicked')
+    this.router.navigate(['/admin']);
+   
+  }
+
+
 }
