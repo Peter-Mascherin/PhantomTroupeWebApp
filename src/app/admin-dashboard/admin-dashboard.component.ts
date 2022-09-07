3db36ff0edb54fd2cb4c07c767cfb6b45cc0c6e7
abdulnmo <abdulnmo@sheridancollege.ca>
2022-09-07 18:22:05 -0400

    auth added

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormServiceService } from '../service/form-service.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private service: FormServiceService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.data.subscribe(data=> {
+      console.log("User logged in") //delete after production
+    })
+  }
+
+  userLogOut(){
+    console.log("user logging out")
+    this.service.performLogOutAction()
   }
 
 }
